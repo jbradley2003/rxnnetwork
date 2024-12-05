368a7be7fd506fe19ce670d4f5febc02f41248e1
Jeff Wang <jeffwang114@gmail.com>
2024-12-04 21:09:56 -0600

    completed conversion matrices for 1 molecular graph and 1 iteration

--- conversion.py
+++ conversion.py
@@ -1,63 +1,19 @@
 import numpy as np
 
-def generate_conversion_matrices(reactant_graph, max_bond_changes=2):
+
+def one_cycle_conversion_matrices(molecular_graph, max_bond_changes=2):
     """
-    Generate conversion matrices for a given reactant molecular graph.
+    Generate all conversion matrices for a 1 iteration of a given molecular graph.
 
     Args:
-        reactant_graph (matrix): Atom connectivity (AC) matrix of the reactant.
-        max_bond_changes (int): Maximum number of bond changes (formation/dissociation) per step.
-
-    Returns:
-        list: A list of valid conversion matrices.
-    """
-
-    num_atoms = len(reactant_graph)
-    conversion_matrices = []
-
-    # Iterate over all possible changes in the AC matrix
-    for i in range(num_atoms):
-        for j in range(i + 1, num_atoms):  # Upper triangle of the matrix
-            current_bond = reactant_graph[i][j]
-            # Case 1: Bond formation (0 -> 1)
-            if current_bond == 0:
-                new_matrix1 = np.copy(reactant_graph)
-                new_matrix2 = np.copy(reactant_graph)
-                
-                new_matrix1[i][j] = 1  # Form bond
-                new_matrix1[j][i] = 1  # Symmetric update
-                if is_valid_matrix(reactant_graph, new_matrix1, max_bond_changes):
-                    conversion_matrices.append(new_matrix1)
-                    
-                new_matrix2[i][j] = 0  # Form bond
-                new_matrix2[j][i] = 0  # Symmetric update
-                if is_valid_matrix(reactant_graph, new_matrix2, max_bond_changes):
-                    conversion_matrices.append(new_matrix2)
-            # Case 2: Bond cleavage (1 -> -1)
-            elif current_bond == 1:
-                new_matrix1 = np.copy(reactant_graph)
-                new_matrix1[i][j] = -1  # Break bond
-                new_matrix1[j][i] = -1  # Symmetric update
-                if is_valid_matrix(reactant_graph, new_matrix1, max_bond_changes):
-                    conversion_matrices.append(new_matrix1)
-    return conversion_matrices
-
-
-# testing another version
-def generate_conversion_matrices_v1(reactant_graph, max_bond_changes=2):
-    """
-    Generate conversion matrices for a given reactant molecular graph.
-
-    Args:
-        reactant_graph (numpy matrix): Atom connectivity (AC) matrix of the reactant.
+        molecular_graph (numpy matrix): Atom connectivity (AC) matrix of the starting molecular graph.
         max_bond_changes (int): Maximum number of bond changes allowed (formation/dissociation) per step.
 
     Returns:
-        list: A list of valid conversion matrices.
+        list: A list of valid conversion numpy matrices.
     """
 
-    num_atoms = reactant_graph.shape[0]
-    print(num_atoms)
+    num_atoms = molecular_graph.shape[0]
 
     # Iterate over non-diagonal upper triangular matrix indices
     # store those indices in a list
@@ -67,31 +23,49 @@ def generate_conversion_matrices_v1(reactant_graph, max_bond_changes=2):
         for j in range(i + 1, num_atoms):  # Upper triangle of the matrix
             A_ij_list.append((i,j))
 
-    print(A_ij_list)
     
     permutation_size = len(A_ij_list)
     permutation_list = [1]*max_bond_changes + [0]*(permutation_size-max_bond_changes)
-    unique_permutations = permuteUnique(permutation_list)
+    unique_permutations = permute_unique(permutation_list)
     
     # form the permutation matrices
     # 1. for each permutation list
     # 2. match the index of the value in the permutation list with the indices in the adjacency list
     conversion_matrices = []
     for matrix in unique_permutations:
-        temp = np.copy(reactant_graph)
+        # create an non-zero upper triangular copy of the reactant graph
+        upper = np.triu(np.copy(molecular_graph))
         for x in range(len(matrix)):
             i = A_ij_list[x][0]
             j = A_ij_list[x][1]
-            # for each element in each permutation matrix
-            temp[i,j] = matrix[x]
-        conversion_matrices.append(temp)
+            
+            # if in the reactant graph it is 1
+            # and if in our permutation matrix it is 1
+            # switch to -1
+            if molecular_graph[i,j] == 1 and matrix[x] == 1:
+                upper[i,j] = -1
+            else: 
+                upper[i,j] = matrix[x]
+        
+        # add in the lower symmetric component
+        lower = upper.T
+        conversion = upper + lower
+        conversion_matrices.append(conversion)
     
     return conversion_matrices
 
 
 
 
-def permuteUnique(nums):
+def permute_unique(nums):
+    """ Creates all unique permutations from a list
+
+    Args:
+        nums (list): list of values that we want to create permutations for
+
+    Returns:
+        list of list: list of the permutation lists
+    """
     res = []
     n = len(nums)
     visited = [False] * n
@@ -113,44 +87,29 @@ def permuteUnique(nums):
     return res
 
 
-
-
-
-def is_valid_matrix(original_matrix, new_matrix, max_bond_changes):
-    """
-    Check if a matrix is valid based on maximum bond changes.
+def print_arrays(array_list):
+    """prints out the numpy arrays so we can see them better
 
     Args:
-        original_matrix (matrix): The original AC matrix.
-        new_matrix (matrix): The AC matrix after changes.
-        max_bond_changes (int): Maximum allowable changes.
-
-    Returns:
-        bool: True if valid, False otherwise.
+        array_list (numpy array): list of numpy arrays
     """
-    # Count the number of changes (formation: 0->1, cleavage: 1->-1)
-    changes = np.sum(np.abs(original_matrix - new_matrix) > 0)
-    return changes <= max_bond_changes
+    for i in array_list:
+        print(i)
+        print()
+
+# ----------------------------- TESTING ----------------------------------------------------
+# ------------------------------------------------------------------------------------------
+r = np.array([[0,1,0,0,0,0],[1,0,0,0,0,0],[0,0,0,1,0,0], [0,0,1,0,0,0],[0,0,0,0,0,1],[0,0,0,0,1,0]])
+
+test_matrix = np.array([[0,1,0],[0, 0, 1],[0, 0, 0]])
+
+test2_matrix = np.empty((5,5))
+result = one_cycle_conversion_matrices(test_matrix)
+
+print(test_matrix)
+print_arrays(result)
 
 
-
-r = [[0,1,0,0,0,0],[1,0,0,0,0,0],[0,0,0,1,0,0], [0,0,1,0,0,0],[0,0,0,0,0,1],[0,0,0,0,1,0]]
-
-test_matrix = np.array([[0,0,0],[0, 0, 0],[0, 0, 0]])
-test2_matrix = np.empty((20,20))
-print(generate_conversion_matrices_v1(test_matrix))
-
-# test = np.copy(r)
-
-
-# l = generate_conversion_matrices(r)
-
-# m = np.unique(np.array(l),axis=0)
-# i = np.array([]) 
-
-# for val in m:
-#     inter = np.add(test, val)
-#     print(inter)
 
 
 
