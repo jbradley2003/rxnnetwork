d4a0e474b54d01aaf4bdca27009742509b74a652
Jeff Wang <jeffwang114@gmail.com>
2024-12-04 20:13:57 -0600

    first draft of the conversion matrices

--- conversion.py
+++ conversion.py
@@ -43,6 +43,79 @@ def generate_conversion_matrices(reactant_graph, max_bond_changes=2):
     return conversion_matrices
 
 
+# testing another version
+def generate_conversion_matrices_v1(reactant_graph, max_bond_changes=2):
+    """
+    Generate conversion matrices for a given reactant molecular graph.
+
+    Args:
+        reactant_graph (numpy matrix): Atom connectivity (AC) matrix of the reactant.
+        max_bond_changes (int): Maximum number of bond changes allowed (formation/dissociation) per step.
+
+    Returns:
+        list: A list of valid conversion matrices.
+    """
+
+    num_atoms = reactant_graph.shape[0]
+    print(num_atoms)
+
+    # Iterate over non-diagonal upper triangular matrix indices
+    # store those indices in a list
+    A_ij_list = []
+    for i in range(num_atoms-1):
+        # find the unique matrices that only have 1 change
+        for j in range(i + 1, num_atoms):  # Upper triangle of the matrix
+            A_ij_list.append((i,j))
+
+    print(A_ij_list)
+    
+    permutation_size = len(A_ij_list)
+    permutation_list = [1]*max_bond_changes + [0]*(permutation_size-max_bond_changes)
+    unique_permutations = permuteUnique(permutation_list)
+    
+    # form the permutation matrices
+    # 1. for each permutation list
+    # 2. match the index of the value in the permutation list with the indices in the adjacency list
+    conversion_matrices = []
+    for matrix in unique_permutations:
+        temp = np.copy(reactant_graph)
+        for x in range(len(matrix)):
+            i = A_ij_list[x][0]
+            j = A_ij_list[x][1]
+            # for each element in each permutation matrix
+            temp[i,j] = matrix[x]
+        conversion_matrices.append(temp)
+    
+    return conversion_matrices
+
+
+
+
+def permuteUnique(nums):
+    res = []
+    n = len(nums)
+    visited = [False] * n
+    def dfs(path, i):
+        if i == n:
+            res.append(path[:])
+            return
+        for j in range(n):
+            if visited[j]: continue
+            if j > 0 and nums[j] == nums[j-1] and not visited[j-1]:
+                continue
+            path.append(nums[j])
+            visited[j] = True
+            dfs(path, i+1)
+            path.pop()
+            visited[j] = False
+    
+    dfs([], 0)
+    return res
+
+
+
+
+
 def is_valid_matrix(original_matrix, new_matrix, max_bond_changes):
     """
     Check if a matrix is valid based on maximum bond changes.
@@ -60,18 +133,24 @@ def is_valid_matrix(original_matrix, new_matrix, max_bond_changes):
     return changes <= max_bond_changes
 
 
+
 r = [[0,1,0,0,0,0],[1,0,0,0,0,0],[0,0,0,1,0,0], [0,0,1,0,0,0],[0,0,0,0,0,1],[0,0,0,0,1,0]]
 
-test = np.copy(r)
+test_matrix = np.array([[0,0,0],[0, 0, 0],[0, 0, 0]])
+test2_matrix = np.empty((20,20))
+print(generate_conversion_matrices_v1(test_matrix))
 
-l = generate_conversion_matrices(r)
+# test = np.copy(r)
 
-m = np.unique(np.array(l),axis=0)
-i = np.array([]) 
 
-for val in m:
-    inter = np.add(test, val)
-    print(inter)
+# l = generate_conversion_matrices(r)
+
+# m = np.unique(np.array(l),axis=0)
+# i = np.array([]) 
+
+# for val in m:
+#     inter = np.add(test, val)
+#     print(inter)
 
 
 
